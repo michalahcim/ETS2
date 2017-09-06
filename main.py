edd89c0f609df741ef20534c9aee034ec6333696
Michał <michalgawlik3@gmail.com>
2017-09-06 21:11:28 +0200

    wykrywanie do 4 linii pod odpowiednim kątem (nie wykrywa horyzontu)

---
+++ main.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+#program napisany z poradnikiem
+#https://www.youtube.com/watch?v=ks4MPfMq8aQ&list=PLQVvvaa0QuDeETZEOy4VdocT7TOjfSA8a
+#jednak nie jest to kopia 1:1
+
+import numpy as np
+from PIL import ImageGrab
+import cv2
+import time
+import math
+from directkeys import ReleaseKey, PressKey, W, A, S, D 
+
+
+def draw_lines(img,lines):
+    zmniejsz = 0 #zeby wyswietlac tylko 2 linie z dodatniego lub ujemnego nachylenia
+    wieksz = 0
+    try:
+        for line in lines:
+            coords = line[0]
+            a = (coords[3]-coords[1])/(coords[2]-coords[0])
+            radian = np.arctan(a)
+            stopnie = math.degrees(radian)
+            print(stopnie)
+            if stopnie <= -7:
+                if zmniejsz >=2:
+                    break
+                else:
+                    zmniejsz = zmniejsz + 1
+                    cv2.line(img, (coords[0], coords[1]), (coords[2], coords[3]), [255,255,255], 3)
+            elif stopnie >=7:
+                if wieksz >=2:
+                    break
+                else:
+                    wieksz = wieksz + 1
+                    cv2.line(img, (coords[0], coords[1]), (coords[2], coords[3]), [255,255,255], 3)
+    except:
+        pass
+
+
+def process_img(original_image):
+    processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    processed_img = cv2.Canny(processed_img, threshold1=50, threshold2=100)
+    vertices = np.array([[117,407],[150,0],[650,0],[683,407],], np.int32) #dla widoku z gory
+
+    processed_img = cv2.GaussianBlur(processed_img,(5,5),0)
+    processed_img = roi(processed_img, [vertices])
+   
+    lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180, 1,  150) #min_dlugosc, max_przerwa
+    draw_lines(processed_img,lines)
+    return processed_img
+
+
+def roi(img, vertices):
+    #blank mask:
+    mask = np.zeros_like(img)
+    # fill the mask
+    cv2.fillPoly(mask, vertices, 255)
+    # now only show the area that is the mask
+    masked = cv2.bitwise_and(img, mask)
+    return masked
+
+
+def main():
+    last_time = time.time()
+    while(True):
+        screen =  np.array(ImageGrab.grab(bbox=(0,40, 800, 640)))
+        new_screen = process_img(screen)
+        print('Loop took {} seconds'.format(time.time()-last_time))
+        last_time = time.time()
+        cv2.imshow('window', new_screen)
+        #cv2.imshow('window2', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
+main()
