a7fde4c40413c4df38c527d709a451d074a46538
Michał <michalgawlik3@gmail.com>
2017-09-09 12:21:40 +0200

    nagrywanie ruchow

--- main.py
+++ main.py
@@ -10,148 +10,60 @@ import cv2
 import time
 import ctypes
 import math
-import pyautogui
+from grabscreen import grab_screen
 import win32api
-from directkeys import ReleaseKey, PressKey, W, A, S, D 
+from uczenie_getkeys import key_check
+
+import os
 
 
-def prosto():
-    ReleaseKey(A)
-    ReleaseKey(D)
-    #pyautogui.moveTo(x,y)
-    #ctypes.windll.user32.mouse_event(0, x, y, 0,0)
-    global i_l
-    global i_p
-    i_l=i_p=0
-    print("Prosto")
+def keys_to_output(keys):
+    '''
+    Convert keys to a ...multi-hot... array
 
-def lewo():
-    PressKey(A)
-    ReleaseKey(D)
-    #pyautogui.moveTo(x,y)
-    #pyautogui.moveRel(x-10,0)
-    #x= x_st
-    #x = x+1
-    #ctypes.windll.user32.mouse_event(0, x, y, 0,0)
-    #print(x)
-    global i_l
-    i_l = i_l + 1
-    print("lewo")
-
-def prawo():
-    #pyautogui.moveTo(x,y)
-    #pyautogui.moveRel(x+10,)
-    PressKey(D)
-    ReleaseKey(A)
-    #x = x_st
-    #x = x-1
-    #ctypes.windll.user32.mouse_event(0, x, y, 0,0)
-    global i_p
-    i_p = i_p + 1
-    print("prawo")
+    [A,W,D] boolean values.
+    '''
+    output = [0,0,0]
     
-def stop():
-    #ReleaseKey(A)
-    #ReleaseKey(D)
-    PressKey(S)
+    if 'A' in keys:
+        output[0] = 1
+    elif 'D' in keys:
+        output[2] = 1
+    else:
+        output[1] = 1
+    return output
 
-def draw_lines(img,lines):
-    global zmniejsz #zeby wyswietlac tylko 2 linie z dodatniego lub ujemnego nachylenia
-    global wieksz 
-    zmniejsz = 0
-    wieksz = 0
-    kat = 15
-    try:
-        for line in lines:
-            coords = line[0]
-            a = (coords[3]-coords[1])/(coords[2]-coords[0])
-            radian = np.arctan(a)
-            stopnie = math.degrees(radian)
-           # print(stopnie)
-            if stopnie <= -(kat):   #to jest po to zeby nie wykrywalo horyzontu i linii poziomych
-                if zmniejsz >=1:
-                    break
-                else:
-                    zmniejsz = zmniejsz + 1
-                    cv2.line(img, (coords[0], coords[1]), (coords[2], coords[3]), [255,255,255], 3)#wyswietlanie lini
-            elif stopnie >=kat:
-                if wieksz >=1:
-                    break
-                else:
-                    wieksz = wieksz + 1
-                    cv2.line(img, (coords[0], coords[1]), (coords[2], coords[3]), [255,255,255], 3)
-    except:
-        pass
+file_name = "training_data.npy"
 
-
-def process_img(original_image):
-    processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY) #zamiana kolorow na grey
-    processed_img = cv2.Canny(processed_img, threshold1=50, threshold2=100) #zamiana obrazu na takie kreski
-    #vertices = np.array([[117,407],[150,0],[650,0],[683,407],], np.int32) #dla widoku z gory
-    vertices = np.array([[0,640],[0,540],[160,310],[640,310],[800,540],[800,640]], np.int32) #dla widoku z maski
+if os.path.isfile(file_name):
+    print("File exists")
+    training_data = list(np.load(file_name))
+else:
+    print("Starting fresh")
+    training_data = []
     
 
-    processed_img = cv2.GaussianBlur(processed_img,(5,5),0) #dodanie blura zeby poprawic wykrywanie linii
-    processed_img = roi(processed_img, [vertices])
-   
-    lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 15, 10,  150) #min_dlugosc, max_przerwa
-    draw_lines(processed_img,lines)
-    return processed_img
-
-
-def roi(img, vertices): #ograniczanie pola obrazu
-    #blank mask:
-    mask = np.zeros_like(img)
-    # fill the mask
-    cv2.fillPoly(mask, vertices, 255)
-    # now only show the area that is the mask
-    masked = cv2.bitwise_and(img, mask)
-    return masked
-
-class POINT(ctypes.Structure):
-    _fields_ = [("x", ctypes.c_ulong), ("y", ctypes.c_ulong)]
-
-def cursorPos():
-    point = POINT()
-    ctypes.windll.user32.GetCursorPos(ctypes.pointer(point))
-    return (point.x, point.y)
 
 def main():
-    global i_l
-    global i_p
-    last_time = time.time()
+    print("start za 5 sek")
+    time.sleep(5)
+    
+    #last_time = time.time()
     while(True):
-        screen =  np.array(ImageGrab.grab(bbox=(0,40, 800, 640)))
-        new_screen = process_img(screen)
-        print('Loop took {} seconds'.format(time.time()-last_time))
-        last_time = time.time()
-        cv2.imshow('window', new_screen)
-        #cv2.imshow('window2', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
-        if wieksz and zmniejsz ==1:
-            for i in range(5):#zeby zignorowac lekkie skrety na prostej drodze 
-                prosto()
-               
-        elif wieksz ==1 and i_l <=5:
-            lewo()
-        elif zmniejsz ==1 and i_p<=5:
-            prawo()
-           
-        else:
-             for i in range(3):#zeby zignorowac lekkie skrety na prostej drodze 
-                prosto()
+        screen =  grab_screen(region = (0,40,800,640))
+        screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
+        screen = cv2.resize(screen,(80,60))
+        keys = key_check()
+        output = keys_to_output(keys)
+        training_data.append([screen, output])
+        #print('Loop took {} seconds'.format(time.time()-last_time))
+        #last_time = time.time()
         
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
-            break
+        if len(training_data)%500 ==0:
+            print(len(training_data))
+            np.save(file_name, training_data)
+       
         
-print("start za 5 sek")
 
-for i in range (0,1):
-    time.sleep(1)
-#x,y = 0,0
-i_l=0
-i_p=0
-#x_st, y_st = x,y 
-#ctypes.windll.user32.SetCursorPos(x, y)
-#pyautogui.dragTo(400,400)
+
 main()
